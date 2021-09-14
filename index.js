85abe0b5ef654d65f6451a4147d40d3aa995d8b4
dylanjenks <dylan.jenks92@gmail.com>
2021-09-14 11:34:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names) {
+    const note = [];
+    for (let i = 0; i < names.length; i++) {
+        note.push( `Thank you, ${names[i]}, for the wonderful surprise gift!` );
+    }
+
+    return note;
+
+}
+
+writeCards(names);
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+countDown(10);
